dbd17a6fa71da76025c9080ab20d5d033f646075
Pray Raskapoorwala <119486563+Martialgod1@users.noreply.github.com>
2024-03-21 01:35:44 +0530

    Update Quark.sql

--- Quark.sql
+++ Quark.sql
@@ -7,7 +7,3 @@ CREATE TABLE IF NOT EXISTS input_data (
     input_str VARCHAR(255) NOT NULL,
     unique_integer VARCHAR(255) NOT NULL PRIMARY KEY
 );
-
-select *from input_data;
-select input_str from input_data where unique_integer="bnF";
-
